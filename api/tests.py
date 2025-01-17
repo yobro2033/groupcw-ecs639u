e2b0e8b2be7e149a1b6d34b610aa8dba01bb5806
yobro2033 <mlhmytran@gmail.com>
2025-01-17 09:13:45 +0000

    added tests

--- api/tests.py
+++ api/tests.py
@@ -3,7 +3,7 @@ import time
 from django.test import TestCase
 import requests, names, random
 
-#from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
@@ -164,3 +164,227 @@ def seach_users():
 
     response = requests.get('http://127.0.0.1:8000/api/users/', params=params, cookies=cookies, headers=headers)
     print(response.text)
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    fixtures=['api/fixtures/test_data.json']
+    
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.driver1 = WebDriver()
+        cls.driver1.implicitly_wait(10)
+        
+        cls.driver2 = WebDriver()
+        cls.driver2.implicitly_wait(10)
+        
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver1.quit()
+        cls.driver2.quit()
+        super().tearDownClass()
+
+
+    # Performs the sign up process
+    def _signup(self, firstname, lastname, email, DOB, password, driver):
+
+        # Accessing the sign up page
+        signup = driver.find_element(By.LINK_TEXT, "Sign Up")
+        signup.click()
+
+        firstname_input = driver.find_element(By.ID, "id_first_name")
+        lastname_input = driver.find_element(By.ID, "id_last_name")
+        email_input = driver.find_element(By.ID, "id_email")
+        DOB_input = driver.find_element(By.ID, "id_date_of_birth")
+        password_input = driver.find_element(By.ID, "id_password1")
+        confirmPassword_input = driver.find_element(By.ID, "id_password2")
+        
+        firstname_input.send_keys(firstname)
+        time.sleep(0.5)
+        lastname_input.send_keys(lastname)
+        time.sleep(0.5)
+        email_input.send_keys(email)
+        time.sleep(0.5)
+        DOB_input.send_keys(DOB)
+        time.sleep(0.5)
+        password_input.send_keys(password)  
+        time.sleep(0.5)
+        confirmPassword_input.send_keys(password)
+        time.sleep(0.5)
+        confirmPassword_input.submit()
+        
+
+
+    # Performs the login process
+    def _login(self, username, password, driver):
+
+        #accessing the login page
+        login = driver.find_element(By.LINK_TEXT, "Login")
+        login.click()
+        time.sleep(1)
+
+        email_input = driver.find_element(By.ID, "id_username")
+        password_input = driver.find_element(By.ID,"id_password")
+       
+        email_input.send_keys(username)
+        time.sleep(0.5)
+        password_input.send_keys(password)
+        time.sleep(0.5)
+        password_input.submit()
+
+    # Performs the logout process
+    def _logout(self, driver):
+        logout = driver.find_element(By.XPATH, "/html/body/div/main/div/div[1]/nav/div/div/ul/li[5]")
+        logout.click()
+
+
+    # Performs the editing of user information
+    def _editing_info(self, first, last, email, DOB, hobby, driver):
+
+        firstnameChange = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[1]/div/div[2]/input")
+        lastnameChange = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[1]/div/div[3]/input")
+        emailChange = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[1]/div/div[4]/input")
+        DOBChange = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[1]/div/div[5]/input")
+        HobbyTest = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[1]/div/div[6]/div")
+        submit = driver.find_element(By.CLASS_NAME, "btn-primary")
+
+        firstnameChange.clear()
+        firstnameChange.send_keys(first)
+        time.sleep(0.5)
+        lastnameChange.clear()
+        lastnameChange.send_keys(last)
+        time.sleep(0.5)
+        emailChange.clear()
+        emailChange.send_keys(email)
+        time.sleep(0.5)
+        DOBChange.clear()
+        DOBChange.send_keys(DOB)
+        time.sleep(0.5)
+        HobbyTest.send_keys(f"{hobby}\ue007")
+        time.sleep(0.5)
+        HobbyDescription = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[1]/div/div[6]/div[2]/input[2]")
+        HobbyDescription.send_keys(f"This is the activity of {hobby}")
+        time.sleep(0.5)
+        hobbySubmit = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[1]/div/div[6]/div[2]/button")
+        time.sleep(2)
+        hobbySubmit.click()
+
+
+        time.sleep(2)
+        submit.click()
+ 
+
+    # Performs the changing of password
+    def _changing_password(self, oldPass, newPass, driver):
+        changePassword = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[2]/button")
+        changePassword.click()
+        time.sleep(1)
+
+        currentPassword = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[3]/div/div/div[2]/div[1]/input")
+        newPassword = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[3]/div/div/div[2]/div[2]/input")
+        confirmPassword = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[3]/div/div/div[2]/div[3]/input")
+        submit = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[3]/div/div/div[3]/button[1]")
+
+        currentPassword.send_keys(oldPass)
+        time.sleep(0.5)
+        newPassword.send_keys(newPass)
+        time.sleep(0.5)
+        confirmPassword.send_keys(newPass)
+        time.sleep(0.5)
+        submit.click()
+        time.sleep(2)
+
+
+    # Performs the filtering of users by age
+    def _filtering_age(self, driver):
+        search = driver.find_element(By.LINK_TEXT, "Search")
+        search.click()
+        time.sleep(1)
+
+        ageSliderLower = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[1]/div/div/input[1]")
+        ageSliderUpper = driver.find_element(By.XPATH, "/html/body/div/main/div/div[2]/div[1]/div/div/input[2]")
+
+        for i in range(8):
+            ageSliderLower.send_keys(Keys.RIGHT)
+        
+        for i in range(30):
+            ageSliderUpper.send_keys(Keys.LEFT)
+
+
+
+    # Performs the sending of a friend request
+    def _send_friend_request(self, driver):
+        sendRequest = driver.find_element(By.CLASS_NAME, "btn-primary")
+        sendRequest.click()
+
+   
+    # Performs the receiving of a friend request
+    def _receive_friend_request(self, driver):
+        requests = driver.find_element(By.XPATH, "/html/body/div/main/div/div[1]/nav/div/div/ul/li[1]/div/button")
+        requests.click()
+        time.sleep(1)
+
+        receiveRequest = driver.find_element(By.CLASS_NAME, "btn-success")
+        receiveRequest.click()
+        time.sleep(1)
+
+
+    # Performs the showing of current friends
+    def _show_friends(self, driver):
+        currentFriends = driver.find_element(By.XPATH, "/html/body/div/main/div/div[1]/nav/div/div/ul/li[2]/div/button")
+        currentFriends.click()
+
+
+    # Performs the live testing of the application using selenium
+    def test_liveserver(self):
+        self.driver1.get(f"{self.live_server_url}")
+        time.sleep(2)
+
+        # Creating an account
+        self._signup("James", "Smith", "Jamessmith@test.com", "10-09-1987", "seleniumPassword", self.driver1)
+        time.sleep(2)
+
+        # Changing the user's details 
+        self.driver1.find_element(By.LINK_TEXT, "Profile Page").click()
+        self._editing_info("Josh", "Brown", "ChangedEmail@test.com", "17-02-1984", "Running", self.driver1)
+        time.sleep(2)
+
+        # Changing the password
+        self._changing_password("seleniumPassword", "passwordSelenium", self.driver1)
+        time.sleep(2)
+
+        # Logging out of the account just created
+        self._logout(self.driver1)
+        time.sleep(2)
+
+        # Logging back in to the account 
+        self._login("ChangedEmail@test.com", "passwordSelenium", self.driver1)
+        time.sleep(2)
+
+        # Search and filetering by age
+        self._filtering_age(self.driver1)
+        time.sleep(2)
+
+        # Sending a friend request
+        self._send_friend_request(self.driver1)
+        time.sleep(2)
+        
+        # Returning to the home page
+        self.driver1.find_element(By.LINK_TEXT, "Home").click()
+
+        # Creating second browser
+        self.driver2.get(f"{self.live_server_url}")
+        time.sleep(2)
+
+        # Logging in to second account
+        self._login("Tomstar@testmail.com", "passwordTesting", self.driver2)
+        time.sleep(2)
+
+        # Accepting friend request
+        self._receive_friend_request(self.driver2)
+        time.sleep(2)
+
+        # Showing current friends
+        self._show_friends(self.driver1)
+        self._show_friends(self.driver2)
+        time.sleep(3)
